b2bd70b342c21bde7f07720dcda60564c2ea45a3
Aighangbe18 <biaghangbe@gmail.com>
2025-10-03 23:25:01 +0100

    1st entry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+// app/_layout.tsx
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+export default function AppLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
+      {/* Onboarding flow */}
+      <Stack.Screen name="index" />
+
+      {/* Authentication flow */}
+      <Stack.Screen name="(Auth)" />
+
+      {/* Main app flow (bottom tabs) */}
+      <Stack.Screen name="(Main)" />
+    </Stack>
   );
 }
